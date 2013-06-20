d909612f39b8e9e283f32d7aa1b1405b99481fc6
Mariusz Nowak <mariusz@medikoo.com>
2013-06-20 13:38:45 +0200

    Update up to new extensions handling in Webmake

--- index.js
+++ index.js
@@ -5,7 +5,5 @@ var parse = require('js-yaml/lib/js-yaml/loader').load
   , stringify = JSON.stringify;
 
 exports.extension = 'yaml';
-exports.noDependencies = true;
-exports.compile = function (src) {
-	return { code: 'module.exports = ' + stringify(parse(src)) + ';' };
-};
+exports.type = 'json';
+exports.compile = function (src) { return { code: stringify(parse(src)) }; };
